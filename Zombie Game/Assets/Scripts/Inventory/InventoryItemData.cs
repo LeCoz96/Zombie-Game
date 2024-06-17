cd4019938e318e76580292afbc08a4aa0b68ec1f
LukyLewis2 <lewis.cozens@hotmail.co.uk>
2024-06-17 22:27:39 +0100

    # Test push

---
+++ Zombie Game/Assets/Scripts/Inventory/InventoryItemData.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItemData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // https://www.youtube.com/watch?v=SGz3sbZkfkg&t=509s
+}
